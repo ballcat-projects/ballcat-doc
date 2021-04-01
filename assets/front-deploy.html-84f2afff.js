b62648a964265b3d89c2ea6992f7d368635c571a
hccake <chengbohua@foxmail.com>
2021-04-01 17:55:21 +0800

    :tada: github page 部署文档

---
+++ assets/front-deploy.html-84f2afff.js
@@ -0,0 +1,43 @@
+import{_ as n,o as s,c as a,e}from"./app-c2dc8116.js";const p={},i=e(`<h1 id="前端部署" tabindex="-1"><a class="header-anchor" href="#前端部署" aria-hidden="true">#</a> 前端部署</h1><h2 id="代码构建" tabindex="-1"><a class="header-anchor" href="#代码构建" aria-hidden="true">#</a> 代码构建</h2><p>只需要在前端项目根路径执行如下命令之一，即可完成构建。</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>$ <span class="token function">yarn</span> build
+or
+$ <span class="token function">npm</span> run build
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>构建打包完成后，将会在项目根目录生成 <code>dist</code> 文件夹，其中即是构建好的静态文件，上传此文件夹到服务器的指定位置即可。</p><h2 id="nginx-配置" tabindex="-1"><a class="header-anchor" href="#nginx-配置" aria-hidden="true">#</a> Nginx 配置</h2><p>示例配置如下，可按需调整：</p><div class="language-nginx line-numbers-mode" data-ext="nginx"><pre class="language-nginx"><code><span class="token directive"><span class="token keyword">server</span></span> <span class="token punctuation">{</span>
+    <span class="token directive"><span class="token keyword">listen</span>  <span class="token number">80</span></span><span class="token punctuation">;</span>  <span class="token comment"># 监听 80 端口</span>
+    <span class="token directive"><span class="token keyword">server_name</span> preview.ballcat.cn</span><span class="token punctuation">;</span> <span class="token comment"># 匹配的域名</span>
+
+    <span class="token comment"># gzip 压缩配置，建议开启</span>
+    <span class="token directive"><span class="token keyword">gzip</span> <span class="token boolean">on</span></span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">gzip_min_length</span> <span class="token number">1k</span></span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">gzip_buffers</span> <span class="token number">4</span> <span class="token number">16k</span></span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">gzip_comp_level</span> <span class="token number">6</span></span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">gzip_types</span> text/plain application/x-javascript text/css application/xml text/javascript application/x-httpd-php image/jpeg image/gif image/png</span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">gzip_disable</span> <span class="token string">&quot;MSIE [1-6]\\.&quot;</span></span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">gzip_vary</span> <span class="token boolean">on</span></span><span class="token punctuation">;</span>	
+
+    <span class="token comment"># 根路径跳转前端打包好的 dist 文件目录</span>
+    <span class="token directive"><span class="token keyword">location</span> /</span> <span class="token punctuation">{</span>
+        <span class="token directive"><span class="token keyword">root</span> /usr/local/ballcat/dist</span><span class="token punctuation">;</span> <span class="token comment"># 这里是服务端 dist 文件夹路径</span>
+        <span class="token directive"><span class="token keyword">index</span> index.html</span><span class="token punctuation">;</span>
+        <span class="token comment"># 用于配合 router History 模式使用</span>
+        <span class="token directive"><span class="token keyword">try_files</span> <span class="token variable">$uri</span> <span class="token variable">$uri</span>/ /index.html</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+
+    <span class="token comment"># 对当前域名下以 /api 开头的请求进行转发</span>
+    <span class="token directive"><span class="token keyword">location</span> ^~/api/</span> <span class="token punctuation">{</span>
+        <span class="token comment"># 转发到对应的服务端地址，并截取 /api 的前缀</span>
+        <span class="token directive"><span class="token keyword">proxy_pass</span> http://127.0.0.1:8080/</span><span class="token punctuation">;</span>
+        <span class="token directive"><span class="token keyword">proxy_set_header</span>   Host    <span class="token variable">$host</span></span><span class="token punctuation">;</span>
+        <span class="token directive"><span class="token keyword">proxy_set_header</span>   X-Real-IP   <span class="token variable">$remote_addr</span></span><span class="token punctuation">;</span>
+        <span class="token directive"><span class="token keyword">proxy_set_header</span>   X-Forwarded-For <span class="token variable">$proxy_add_x_forwarded_for</span></span><span class="token punctuation">;</span>
+
+        <span class="token comment"># 开启 websocket 则需添加如下配置</span>
+        <span class="token directive"><span class="token keyword">proxy_http_version</span> 1.1</span><span class="token punctuation">;</span>
+        <span class="token directive"><span class="token keyword">proxy_set_header</span> Upgrade <span class="token variable">$http_upgrade</span></span><span class="token punctuation">;</span>
+        <span class="token directive"><span class="token keyword">proxy_set_header</span> Connection <span class="token string">&quot;upgrade&quot;</span></span><span class="token punctuation">;</span>
+        <span class="token directive"><span class="token keyword">proxy_connect_timeout</span> <span class="token number">60s</span></span><span class="token punctuation">;</span>
+        <span class="token directive"><span class="token keyword">proxy_read_timeout</span> <span class="token number">120s</span></span><span class="token punctuation">;</span>
+        <span class="token directive"><span class="token keyword">proxy_send_timeout</span> <span class="token number">120s</span></span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,8),t=[i];function c(o,l){return s(),a("div",null,t)}const r=n(p,[["render",c],["__file","front-deploy.html.vue"]]);export{r as default};
---
+++ assets/syncing-fork.html-a5b82a78.js
@@ -0,0 +1,29 @@
+import{_ as e,o as i,c as a,e as t}from"./app-c2dc8116.js";const s={},n=t(`<h1 id="二开代码同步" tabindex="-1"><a class="header-anchor" href="#二开代码同步" aria-hidden="true">#</a> 二开代码同步</h1><p>服务端代码不建议用户二开，而是以引入的方式进行使用，但是前端代码区别于服务端，开发一定会需要新增相关的业务页面或者组件信息。</p><p>此教程说明了如何在二开后方便的进行和源代码仓库进行同步。</p><blockquote><p>服务端代码如果必须二开，建议不要改动包名，且项目的业务代码在独立的仓库进行开发，方便后续升级</p></blockquote><h2 id="添加远程仓库" tabindex="-1"><a class="header-anchor" href="#添加远程仓库" aria-hidden="true">#</a> 添加远程仓库</h2><p>将源仓库配置为二开仓库的一个远程仓库</p><ul><li><p>在本地仓库中使用 <code>git remote -v</code> 查看远程状态。</p><blockquote><p>本地仓库的 origin 地址在哪都不影响，示例中假定其在 github 上，实际开发时，可能在 gitlab 或者 gitee 上</p></blockquote></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>git remote -v
+# origin  https://github.com/YOUR_USERNAME/YOUR_FORK.git (fetch)
+# origin  https://github.com/YOUR_USERNAME/YOUR_FORK.git (push)
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li>将源仓库添加为本地仓库的一个远程上有仓库</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>git remote add upstream https://github.com/ballcat-projects/ballcat-ui-vue.git
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><ul><li>再次查看状态确认是否配置成功。</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>git remote -v
+# origin    https://github.com/YOUR_USERNAME/YOUR_FORK.git (fetch)
+# origin    https://github.com/YOUR_USERNAME/YOUR_FORK.git (push)
+# upstream  https://github.com/ballcat-projects/ballcat-ui-vue.git (fetch)
+# upstream  https://github.com/ballcat-projects/ballcat-ui-vue.git (push)
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="同步更新" tabindex="-1"><a class="header-anchor" href="#同步更新" aria-hidden="true">#</a> 同步更新</h2><ul><li>从上游仓库 fetch 分支和提交点，传送到本地，并会被存储在一个本地分支 upstream/master <code>git fetch upstream</code></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>git fetch upstream
+# remote: Counting objects: 75, done.
+# remote: Compressing objects: 100% (53/53), done.
+# remote: Total 62 (delta 27), reused 44 (delta 9)
+# Unpacking objects: 100% (62/62), done.
+# From https://github.com/ballcat-projects/ballcat-ui-vue.git
+#  * [new branch]      master     -&gt; upstream/master
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li>切换到本地主分支(如果不在的话) <code>git checkout master</code></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>git checkout master
+# Switched to branch &#39;master&#39;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><ul><li>把 upstream/master 分支合并到本地 master 上，这样就完成了同步，并且不会丢掉本地修改的内容。 <code>git merge upstream/master</code></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>git merge upstream/master
+# Updating a422352..5fdff0f
+# Fast-forward
+#  README                    |    9 -------
+#  README.md                 |    7 ++++++
+#  2 files changed, 7 insertions(+), 9 deletions(-)
+#  delete mode 100644 README
+#  create mode 100644 README.md
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li>这时将合并后的代码推送到自己仓库的远程分支上，即可完成此次同步</li></ul><h2 id="常见问题" tabindex="-1"><a class="header-anchor" href="#常见问题" aria-hidden="true">#</a> 常见问题</h2><p>分支无法合并，控制台打印如下错误信息：</p><div class="language-git line-numbers-mode" data-ext="git"><pre class="language-git"><code>fatal: refusing to merge unrelated histories
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>这一般是因为，你的库并没有源仓库的提交历史，git 认为这两个仓库没有关联，所以拒绝了合并命令。 这时通过添加 <code>--allow-unrelated-histories</code> 参数来允许合并即可。</p><div class="language-git line-numbers-mode" data-ext="git"><pre class="language-git"><code>git merge upstream/master --allow-unrelated-histories
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="原文地址" tabindex="-1"><a class="header-anchor" href="#原文地址" aria-hidden="true">#</a> 原文地址</h2><p>https://gaohaoyang.github.io/2015/04/12/Syncing-a-fork/</p><p>https://docs.github.com/en/github/collaborating-with-issues-and-pull-requests/about-forks</p><p>https://docs.github.com/en/github/collaborating-with-issues-and-pull-requests/configuring-a-remote-for-a-fork</p>`,29),d=[n];function l(r,c){return i(),a("div",null,d)}const o=e(s,[["render",l],["__file","syncing-fork.html.vue"]]);export{o as default};
